0d1f3ee6bf6e959767f7df3edb89005be303ae22
CalciteDragon <calcitedragon@gmail.com>
2025-03-15 20:05:28 -0700

    random UI polishing
    - changed the topbar from using a NavigationBar to a CenterAlignedTopAppBar (Navigation bar is only supposed to be used for bottom bars)
    - shifted around stuff in both navbars to look a little nicer
    - fixed the noPendingExpensesCard
    - other bug fixes/cleanup

--- app/src/main/java/com/example/pineappleexpense/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pineappleexpense/ui/screens/HomeScreen.kt
@@ -3,7 +3,10 @@
 package com.example.pineappleexpense.ui.screens
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,7 +30,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,7 +66,7 @@ fun HomeScreen(navController: NavHostController, viewModel: AccessViewModel, mod
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            val reportCards = reportCardsList (viewModel.pendingReports, navController, viewModel)
+            val pendingReports = reportCardsList (viewModel.pendingReports, navController, viewModel)
             val rejectedReports = reportCardsList (viewModel.rejectedReports, navController, viewModel)
             val expenseCards = expenseCardsList(
                 expenses = expenses,
@@ -84,26 +90,25 @@ fun HomeScreen(navController: NavHostController, viewModel: AccessViewModel, mod
                 navController,
                 viewModel
             )
-            if (expenses.isEmpty()) {
+            if (expenses.isEmpty() && rejectedReports.isEmpty() && pendingReports.isEmpty()) {
                 NoPendingExpensesCard(navController)
-            }
-            //put both the expense cards and report cards in the lazy column
-            // Rejected reports will appear at the top
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(bottom = 80.dp)
-            ) {
-                items(rejectedReports) { reportCard ->
-                    reportCard()
+            } else {
+                //put both the expense cards and report cards in the lazy column
+                // Rejected reports will appear at the top
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(bottom = 80.dp)
+                ) {
+                    items(rejectedReports) { reportCard ->
+                        reportCard()
+                    }
+                    items(pendingReports) { reportCard ->
+                        reportCard()
+                    }
+                    items(expenseCards) { expenseCard ->
+                        expenseCard()
+                    }
                 }
-                items(reportCards) { reportCard ->
-                    reportCard()
-                }
-                items(expenseCards) { expenseCard ->
-                    expenseCard()
-                }
-
-
             }
 
             Button(
@@ -147,46 +152,39 @@ fun NoPendingExpensesCard(navController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(12.dp),
         shape = RoundedCornerShape(16.dp)
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column(
-                modifier = Modifier.weight(1f)
+            Text(
+                text = "You currently have no pending expenses",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Box(
+                modifier = Modifier.height(24.dp).width(100.dp).clickable{
+                    navController.navigate("Camera") {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
             ) {
                 Text(
-                    text = "You currently have no pending expenses",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color.Gray
+                    text = "add a new item",
+                    color = Color(0xFF6200EA),
+                    fontSize = 13.sp
                 )
-                Spacer(modifier = Modifier.height(4.dp))
-                Button(
-                    onClick = {
-                        navController.navigate("Camera") {
-                            launchSingleTop = true
-                            restoreState = true
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                    shape = RoundedCornerShape(16.dp),
-                    border = BorderStroke(1.dp, Color(0xFF6200EA)),
-                    contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp)
-                ) {
-                    Text(
-                        text = "add a new item",
-                        color = Color(0xFF6200EA),
-                        fontSize = 12.sp
-                    )
-                }
             }
         }
     }
 }
+
 
 @Preview
 @Composable

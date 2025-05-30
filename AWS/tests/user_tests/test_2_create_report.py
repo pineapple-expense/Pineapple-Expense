59250af8949a77fac4db9f9743f4578e512dc92a
Callowlock <109322648+Callowlock@users.noreply.github.com>
2025-05-29 21:32:22 -0700

    ordering tests

--- AWS/tests/user_tests/test_2_create_report.py
+++ AWS/tests/user_tests/test_2_create_report.py
@@ -1,8 +1,10 @@
 import requests
 import uuid
+import pytest
 
 API_URL = "https://t6oydoeb76.execute-api.us-east-1.amazonaws.com/dev/user/CreateReport"
 
+@pytest.mark.dependency(name="create_report", depends=["predict_image_1", "predict_image_2"])
 def test_create_report(auth_token):
     headers = {
         "Authorization": f"Bearer {auth_token}",
